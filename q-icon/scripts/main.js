49342f9c1130b8555dd5927ebcad946d22416bf5
Qualve <phantasmal.sea@gmail.com>
2020-07-21 06:52:23 +0600

    change home menu

--- q-icon/scripts/main.js
+++ q-icon/scripts/main.js
@@ -7,13 +7,13 @@ let myButton = document.querySelector("button");
 /*** Display change ***/
 myImage.onclick = function () {
   let mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/q-icon-3.png") {
+  if (mySrc === "q-icon/images/q-icon-3.png") {
     // dark mode
-    myImage.setAttribute("src", "images/q-icon-3-2.png");
+    myImage.setAttribute("src", "q-icon/images/q-icon-3-2.png");
     changeEverything("#000000", "#26a15e9d", "#ffffff", "#e9e892af", "#f00c0cd2", "#49003f");
   } else {
     // light mode
-    myImage.setAttribute("src", "images/q-icon-3.png");
+    myImage.setAttribute("src", "q-icon/images/q-icon-3.png");
     changeEverything("#0d5f2c", "#ec7878", "#000000", "#000000", "#a4ff2db0", "#000000");
   }
 }
@@ -34,25 +34,3 @@ function changeEverything(HBC, BBgC, BC, BBC, HC, HTSC) { //e9e892af
   myHeading.style.color = HC;
   myHeading.style.textShadow = "5px 3px 1px " + HTSC;
 }
-
-/*** User Name ***/
-// function setUserName() {
-//   let myName = prompt("Please enter your name.");
-//   if(!myName) {
-//     setUserName();
-//   } else {
-//     localStorage.setItem("name", myName);
-//     myHeading.innerHTML = "Qualveyal's Icon, " + myName;
-//   }
-// }
-
-// if(!localStorage.getItem("name")) {
-//   setUserName();
-// } else {
-//   let storedName = localStorage.getItem("name");
-//   myHeading.textContent = "Qualveyal's Icon, " + storedName;
-// }
-
-// myButton.onclick = function() {
-//   setUserName();
-// }
