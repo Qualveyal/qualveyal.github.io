918f00e07f56c473c42be9aa1c0ac0acae3a50b6
Qualve <phantasmal.sea@gmail.com>
2020-07-23 00:47:00 +0600

    pritified and add a way to get one molecule to be of a specific colr

--- p5-javaScript/molecules/molecule.js
+++ p5-javaScript/molecules/molecule.js
@@ -1,53 +1,53 @@
 class Molecule {
 
-    constructor(r, w, h, s) {
-        this.r = r;
+  constructor(r, w, h, s) {
+    this.r = r;
 
-        this.FOrigin = 0 + this.r;
-        this.FWidth = w - this.r;
-        this.FHeight = h - this.r;
-        
-        this.x = random(this.FOrigin, this.FWidth);
-        this.y = random(this.FOrigin, this.FHeight);
+    this.FOrigin = 0 + this.r;
+    this.FWidth = w - this.r;
+    this.FHeight = h - this.r;
 
-        this.x_speed = random(-s, s);
-        this.y_speed = random(-s, s);
+    this.x = random(this.FOrigin, this.FWidth);
+    this.y = random(this.FOrigin, this.FHeight);
+
+    this.x_speed = random(-s, s);
+    this.y_speed = random(-s, s);
+  }
+
+  display(red = 255, green = 255, blue = 255, alpha = 175) {
+    noStroke();
+    fill(red, green, blue, alpha);
+    ellipse(this.x, this.y, 2 * this.r);
+  }
+
+  move() {
+    this.x += this.x_speed;
+    this.y += this.y_speed;
+  }
+
+  edge() {
+    if (this.x > this.FWidth || this.x < this.FOrigin) {
+      this.x_speed = -this.x_speed;
     }
-
-    display(red = 255, green = 255, blue = 255, alpha = 175) {
-        noStroke();
-        fill(red, green, blue, alpha);
-        ellipse(this.x, this.y, 2 * this.r);
+    if (this.y > this.FHeight || this.y < this.FOrigin) {
+      this.y_speed = -this.y_speed;
     }
+  }
 
-    move() {
-        this.x += this.x_speed;
-        this.y += this.y_speed;
+  collision(other) {
+    let TX = 0;
+    let TY = 0;
+    let d = dist(this.x, this.y, other.x, other.y);
+
+    TX = Molecules[i].x_speed;
+    TY = Molecules[i].y_speed;
+
+    if (d <= this.r + other.r) {
+      Molecules[i].x_speed = other.x_speed;
+      Molecules[i].y_speed = other.y_speed;
+
+      other.x_speed = TX;
+      other.y_speed = TY;
     }
-
-    edge() {
-        if(this.x > this.FWidth || this.x < this.FOrigin) {
-            this.x_speed = -this.x_speed;
-        }
-        if(this.y > this.FHeight || this.y < this.FOrigin) {
-            this.y_speed = -this.y_speed;
-        }
-    }
-
-    collision(other) {
-        let TX = 0; 
-        let TY = 0;
-        let d = dist(this.x, this.y, other.x, other.y);
-
-        TX = Molecules[i].x_speed;
-        TY = Molecules[i].y_speed;
-
-        if(d <= this.r + other.r){
-            Molecules[i].x_speed = other.x_speed;
-            Molecules[i].y_speed = other.y_speed;
-
-            other.x_speed = TX;
-            other.y_speed = TY;
-        }
-    }
+  }
 }
--- p5-javaScript/molecules/sketch.js
+++ p5-javaScript/molecules/sketch.js
@@ -1,36 +1,45 @@
 let Molecules = [];
 let Grid = [];
-let num = 15;
+let num = 100;
 
 function setup() {
-    createCanvas(400, 400);
+  createCanvas(400, 400);
 
-    for(i = 0; i < num; i++) {
-        Molecules[i] = new Molecule(10, width, height, 3);
-    }
+  for (i = 0; i < num; i++) {
+    Molecules[i] = new Molecule(10, width, height, 3);
+  }
 }
 
 function draw() {
-    background(0);
-    view(1);
+  background(0);
+  view(1);
 
-    for(i = 0; i < Molecules.length; i++) {
-        Molecules[i].edge();
+  for (i = 0; i < Molecules.length; i++) {
+    Molecules[i].edge();
 
-        for(j = i + 1; j < Molecules.length; j++) {
-        Molecules[i].collision(Molecules[j]);
-        }      
-
-        Molecules[i].move();   
-        Molecules[i].display();
+    for (j = i + 1; j < Molecules.length; j++) {
+      Molecules[i].collision(Molecules[j]);
     }
+    Molecules[i].move();
+    Molecules[i].display();
+
+    //to change color of specific molecule
+    // if (i != 0) {
+    //   Molecules[i].move();
+    //   Molecules[i].display();
+    // }
+    // else {
+    //   Molecules[0].move();
+    //   Molecules[0].display();
+    // }
+  }
 }
 
 function view(scl = 1) {
-    translate((width - width*scl)/ 2, (height - height*scl)/ 2);
-    scale(scl);
+  translate((width - width * scl) / 2, (height - height * scl) / 2);
+  scale(scl);
 
-    noFill(); 
-    stroke(255);
-    rect(0, 0, width, height);
+  noFill();
+  stroke(255);
+  rect(0, 0, width, height);
 }
