02ff9d7c896bcdf619adba3febd1cf37eb9ff98e
Qualve <phantasmal.sea@gmail.com>
2020-08-02 11:47:19 +0600

    Changed background format

--- websites/home-page/scripts/main.js
+++ websites/home-page/scripts/main.js
@@ -5,13 +5,16 @@ let myImage = document.querySelector("img");
 let myButton = document.querySelector("button");
 let myHr = document.querySelector("hr");
 
+let borderWhite = ", -1px -1px 0 #fff, 1px -1px 0 #fff, -1px 1px 0 #fff, 1px 1px 0 #fff";
+let borderBlack = ", -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000";
+
 /*** Display change ***/
 myImage.onclick = function () {
   let mySrc = myImage.getAttribute("src");
   if (mySrc === "websites/home-page/images/q-icon-3-light.png") {
     // dark mode
     myImage.setAttribute("src", "websites/home-page/images/q-icon-3-dark.png");
-    changeEverything("background-dark.png", "#000000b0", "#ffffff", "#e9e892af", "#b72121");
+    changeEverything("background-dark.jpg", "#000000b0", "#ffffff", "#e9e892af", "#b72121", borderWhite);
   } else {
     // light mode
     myImage.setAttribute("src", "websites/home-page/images/q-icon-3-light.png");
@@ -19,7 +22,7 @@ myImage.onclick = function () {
   }
 }
 
-function changeEverything(Img, BBgC, BC, BBC, HTSC) { //e9e892af
+function changeEverything(Img, BBgC, BC, BBC, HTSC, Border = "") { //e9e892af
     /* 
   Img = HTML background image
   BBgC = Body-Background-Color 
@@ -31,5 +34,5 @@ function changeEverything(Img, BBgC, BC, BBC, HTSC) { //e9e892af
   myBody.style.backgroundColor = BBgC;
   myBody.style.color = BC;
   myBody.style.border =  "5px solid " + BBC;
-  myHeading.style.textShadow = "0px 0px 10px " + HTSC;
+  myHeading.style.textShadow = "0px 0px 10px " + HTSC + Border;
 }
